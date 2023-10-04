cce3d4d7b5a9fd069712ac1dd95c8aec0a606823
Mateo Quotteri <quotterimateo@gmail.com>
2023-10-03 21:56:01 -0300

    generating alleatory cards

--- juegoDeCartas/juegoDeCartasGaston.js
+++ juegoDeCartas/juegoDeCartasGaston.js
@@ -1,13 +1,28 @@
+const numerosAleatorios = [];
+function generarNumeroAleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  
+  // Generar 4 números aleatorios del 1 al 12
+ 
+
 function generarCartas() {
   const numerosInput = document.getElementById('numeros').value;
   const numerosArray = numerosInput.split(',').map(numero => parseInt(numero.trim(), 10));
 
-  if (numerosArray.length === 4) {
-      for (let i = 1; i <= 4; i++) {
-          const carta = document.getElementById(`carta${i}`);
-          carta.textContent = numerosArray[i - 1];
-      }
-  } else {
-      alert('Por favor, ingrese 4 números separados por comas.');
+
+  for (let i = 0; i < 4; i++) {
+    let numeroAleatorio = generarNumeroAleatorio(1, 12);
+    numerosAleatorios.push(numeroAleatorio);
   }
+
+  
+      for (let i = 0; i <= numerosAleatorios.length; i++) {
+          const carta = document.getElementById(`carta${i}`);
+          carta.textContent = numerosAleatorios[i];
+      }
+      console.log(numerosAleatorios);
+  
 }
+
+window.onload = generarCartas()

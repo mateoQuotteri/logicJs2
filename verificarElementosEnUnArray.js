bbda3661e1a263e442999b6d30776aa133c3ff08
Mateo Quotteri <quotterimateo@gmail.com>
2023-11-05 22:58:06 -0300

    learning object location

---
+++ verificarElementosEnUnArray.js
@@ -0,0 +1,9 @@
+
+
+//Verifiquemos si todos elementos del array son pares
+
+
+let numeros = [2, 4, 6, 8, 10];
+let todosPares = numbers.every(number => number % 2 === 0);
+
+console.log(todosPares); // Output: true

72df54d31eadb4d61f3df04cfbd32dde861d09b4
Mateo Quotteri <quotterimateo@gmail.com>
2023-11-09 13:35:40 -0300

    summing all the products prices

--- carrtitoCompras/script.js
+++ carrtitoCompras/script.js
@@ -1,19 +1,35 @@
 let buttons = document.querySelectorAll(".add-to-cart");
 let items = document.getElementById("items")
+let total = document.getElementById("cart-total")
 
+window.addEventListener("load", ()=> {
+  localStorage.removeItem("datos")
+  localStorage.removeItem("productos")
 
+})
+
+// Cada vez que se clickea en "agregar al carrito" el precio del producto
+// que se agrego al carrito se visualiza en el mini carrito situado en la esquina derecha superior
+//del home
 
 function renderizarItemsEnCarrito() {
     const datosGuardados = JSON.parse(localStorage.getItem("datos"));
 
-    let ultimo = datosGuardados.length;
+        let ultimo = datosGuardados.length;
 
-       let e = datosGuardados[ultimo - 1]
+        let e = datosGuardados[ultimo - 1]
 //    datosGuardados.forEach(e => {
         let newItem = document.createElement("h6")
-        newItem.textContent = e;
+        newItem.textContent = "$"+ e;
 
         items.appendChild(newItem)
+
+        let totalActual = parseInt(total.innerHTML)
+        parseInt(totalActual)
+
+        totalActual+= parseInt(e);
+
+        total.textContent = totalActual
    // });
 }
 
@@ -22,15 +38,20 @@ buttons.forEach((element) => {
     console.log(element.attributes[1].value);
 
     const nuevoDato = element.attributes[1].value
+    const nuevoProducto = element.previousElementSibling.innerHTML;
 
     // Obtiene los datos existentes del localStorage (si los hay)
     const datosGuardados = JSON.parse(localStorage.getItem("datos")) || [];
+    const productosGuardados = JSON.parse(localStorage.getItem("productos")) || [];
 
     // Agrega el nuevo dato al arreglo de datos
     datosGuardados.push(nuevoDato);
+    productosGuardados.push(nuevoProducto)
 
     // Almacena los datos actualizados en el localStorage
     localStorage.setItem("datos", JSON.stringify(datosGuardados));
+    localStorage.setItem("productos", JSON.stringify(productosGuardados));
+
 
 
     renderizarItemsEnCarrito();

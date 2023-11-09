ea40d2cb112101ae21f655adceb9b644ee03d904
Mateo Quotteri <quotterimateo@gmail.com>
2023-11-08 21:50:27 -0300

    for each click the price is showed on the mini cart

--- carrtitoCompras/script.js
+++ carrtitoCompras/script.js
@@ -1,11 +1,39 @@
 let buttons = document.querySelectorAll(".add-to-cart");
+let items = document.getElementById("items")
 
 
 
-buttons.forEach(element => {
-    element.addEventListener("click",(e)=> {
-        console.log(element.attributes[1].value);
-    })
+function renderizarItemsEnCarrito() {
+    const datosGuardados = JSON.parse(localStorage.getItem("datos"));
+
+    let ultimo = datosGuardados.length;
+
+       let e = datosGuardados[ultimo - 1]
+//    datosGuardados.forEach(e => {
+        let newItem = document.createElement("h6")
+        newItem.textContent = e;
+
+        items.appendChild(newItem)
+   // });
+}
+
+buttons.forEach((element) => {
+  element.addEventListener("click", (e) => {
+    console.log(element.attributes[1].value);
+
+    const nuevoDato = element.attributes[1].value
+
+    // Obtiene los datos existentes del localStorage (si los hay)
+    const datosGuardados = JSON.parse(localStorage.getItem("datos")) || [];
+
+    // Agrega el nuevo dato al arreglo de datos
+    datosGuardados.push(nuevoDato);
+
+    // Almacena los datos actualizados en el localStorage
+    localStorage.setItem("datos", JSON.stringify(datosGuardados));
+
+
+    renderizarItemsEnCarrito();
+  });
 });
-
 

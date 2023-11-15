a6cad956f769e7f47a0d7d58d9ea9c3b653bf45e
Mateo Quotteri <quotterimateo@gmail.com>
2023-11-14 23:07:30 -0300

    the magic is happening (just the start)

--- gifsGame/js.js
+++ gifsGame/js.js
@@ -1,37 +1,37 @@
-let image = document.getElementById("img")
-const select = document.getElementById("type")
-window.addEventListener("load",(e)=> {
-    fetch("https://api.giphy.com/v1/gifs/categories?api_key=VFidxXzab1k8WlrIZaIHJRjlOegWJBQw")
-    .then((response)=>{
-        return response.json()
-    })
-    .then((data)=>{
-        console.log(data);
-         //image.src = data.data[4].gif.embed_url
-    })
-})
+let image = document.getElementById("img");
+const select = document.getElementById("type");
 
-let div = document.getElementById("div")
 
-select.addEventListener("change", (e)=> {
-    let type = select.value
-    fetch("https://api.giphy.com/v1/gifs/categories?api_key=VFidxXzab1k8WlrIZaIHJRjlOegWJBQw")
-    .then((response)=>{
-        return response.json()
-    })
-    .then((data)=>{
-        for (let i = 0; i < data.data.length; i++) {
-            console.log(data[i].data.name);
-            if (data[i].data.name === type) {
-                console.log("h");
-                let newGif = data.data[i].gif.embed_url
-                let newIframe = document.createElement("iframe")
-                newIframe.src = newGif
-                div.appendChild(newIframe)
-            }
-            
-        }
-        console.log(data);
-       //  image.src = data.data[4].gif.embed_url
-    })
-})
+document.addEventListener("DOMContentLoaded", function () {
+    // Mover la selección del elemento .div aquí
+    
+    select.addEventListener("change", (e) => {
+        let type = select.value;
+        console.log(type);
+        fetch(
+            "https://api.giphy.com/v1/gifs/categories?api_key=VFidxXzab1k8WlrIZaIHJRjlOegWJBQw"
+            )
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                console.log(data);
+                for (let i = 0; i < data.data.length; i++) {
+                    const div = document.querySelector(".div");
+                    if (data.data[i].name === type) {
+                        console.log("h");
+                        let body = document.getElementById("body")
+                        console.log(body);
+                        let newIframe = document.createElement("iframe");
+                        doingMagic(data.data[i].gif.embed_url, newIframe, body);
+                    }
+                }
+            });
+        });
+    });
+    
+    function doingMagic(content, element , dad) {
+        let newGif = content;
+        element.src = newGif;
+        dad.appendChild(element)
+    }
